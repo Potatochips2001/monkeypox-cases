7a5322a1b13619e6edb1399e2372c4ec5ddd8804
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2022-08-23 12:51:17 -0400

    Made it better

--- monkeypox.py
+++ monkeypox.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 
-import requests
+import requests, json, sys
 
-uri = "https://www.cdc.gov/poxvirus/monkeypox/response/2022/us-map.html"
-match = "<span class=\"fs12 text-primary\"> "
+#REMOVED OLD
+#uri = "https://www.cdc.gov/poxvirus/monkeypox/response/2022/us-map.html"
+#match = "<span class=\"fs12 text-primary\"> "
 
-req = requests.get(uri).text
-loc = req.find(match) + len(match)
-removeFirst = req[loc:]
-cases = removeFirst[:removeFirst.find("<")]
-print(f"{cases} USA cases")
+#req = requests.get(uri).text
+#loc = req.find(match) + len(match)
+#removeFirst = req[loc:]
+#cases = removeFirst[:removeFirst.find("<")]
+#print(f"{cases} USA cases")
+
+uri = "https://www.cdc.gov/poxvirus/monkeypox/modules/data-viz/mpx_US_Total_databite.json"
+
+d = requests.get(uri); d = json.loads(d.text)
+totalCases = d['data'][46]['Cases']
+
+enum = 0
+states = {}
+for key in d['data']:
+    states[enum] = key['Location']
+    enum += 1
+
+for state in states:
+    print(f"{d['data'][state]['Location']}: {d['data'][state]['Cases']}")
+
+print(f"\nTotal monkeypox cases: {totalCases:,}")
+

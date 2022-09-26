9311531561d128678448218d951a61ec5aa8bedf
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2022-09-25 22:52:06 -0400

    I made it better

--- monkeypox.py
+++ monkeypox.py
@@ -1,30 +1,48 @@
 #!/usr/bin/python3
 
-import requests, json, sys
+import requests, sys, argparse, math #json is no longer needed
 
-#REMOVED OLD
-#uri = "https://www.cdc.gov/poxvirus/monkeypox/response/2022/us-map.html"
-#match = "<span class=\"fs12 text-primary\"> "
 
-#req = requests.get(uri).text
-#loc = req.find(match) + len(match)
-#removeFirst = req[loc:]
-#cases = removeFirst[:removeFirst.find("<")]
-#print(f"{cases} USA cases")
+#This uri hasn't been updated for a long time, so I'm using another one I was able to find.
+#uri = "https://www.cdc.gov/poxvirus/monkeypox/modules/data-viz/mpx_US_Total_databite.json"
 
-uri = "https://www.cdc.gov/poxvirus/monkeypox/modules/data-viz/mpx_US_Total_databite.json"
+uri = "https://www.cdc.gov/wcms/vizdata/poxvirus/monkeypox/data/MPX-Cases-Deaths-by-Country.csv"
 
-d = requests.get(uri); d = json.loads(d.text)
-totalCases = d['data'][46]['Cases']
+country = ""
 
-enum = 0
-states = {}
-for key in d['data']:
-    states[enum] = key['Location']
-    enum += 1
+parser = argparse.ArgumentParser()
+parser.add_argument("--country")
+args = parser.parse_args()
 
-for state in states:
-    print(f"{d['data'][state]['Location']}: {d['data'][state]['Cases']}")
+if args.country == None:
+    country = ""
+else:
+    country = args.country.lower() + ','
 
-print(f"\nTotal monkeypox cases: {totalCases:,}")
+d = requests.get(uri)
+packet_data = d.text.lower()
+
+if country == "":
+    country = "united states,"
+    usa_cases_t1 = packet_data[packet_data.find(country)+len(country):]
+    usa_cases = usa_cases_t1[:usa_cases_t1.find(",")]
+    usa_deaths_t1 = packet_data[packet_data.find(country)+len(country)+len(usa_cases)+1:]
+    usa_deaths = usa_deaths_t1[:usa_deaths_t1.find(",")]
+    fatal_percentage = int(usa_deaths) / int(usa_cases)
+    exit(f"cases in {country} {int(usa_cases):,}, deaths {int(usa_deaths):,}\nfatality rate: {math.floor(fatal_percentage*1000)/10}%")
+else:
+    try:
+        country_cases_t1 = packet_data[packet_data.find(country)+len(country):]
+        country_cases = country_cases_t1[:country_cases_t1.find(',')]
+        country_deaths_t1 = packet_data[packet_data.find(country)+len(country)+len(country_cases)+1:]
+        country_deaths = country_deaths_t1[:country_deaths_t1.find(',')]
+        if country_deaths == 0:
+            fatal_percentage = 0
+        else:
+            fatal_percentage = int(country_deaths) / int(country_cases)
+        exit(f"cases in {country} {int(country_cases):,}, deaths {int(country_deaths):,}\nfatality rate: {math.floor(fatal_percentage*1000)/10}%")
+    except Exception:
+        exit("country not found")
+
+
 
